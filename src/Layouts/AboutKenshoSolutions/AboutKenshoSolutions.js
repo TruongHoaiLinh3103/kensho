584cf453641c52665cb23da682bcc57db315f35b
Anikey <eagleremlinh@gmail.com>
2023-09-28 23:08:05 +0700

    F

---
+++ src/Layouts/AboutKenshoSolutions/AboutKenshoSolutions.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../../Style/aboutkenshosolutions.css";
+const AboutKenshoSolutions = () => {
+    return (
+        <div className='AboutKenshoSolutions'>
+            <div className='maxWidth1200px AboutKenshoSolutions-body'>
+                <h3>About Kensho Solutions</h3>
+                <p>Kensho pairs the latest techniques in machine learning with the unparalleled breadth and depth of data at S&P Global to give customers comprehensive, timely, actionable insights, empowering them to make decisions with conviction.</p>
+                <p>The world currently has more than a billion knowledge workers spanning hundreds of industries, and those workers deal with a massive uninterrupted flow of unstructured data requiring highly time-intensive manual processing. Workers are overwhelmed and miss insights.</p>
+                <p>Kensho solves this problem by building foundational tools that unlock the hard-to-get data and insights organizations and individuals need to make decisions with conviction. Users gain access to timely insights using our ML Toolkit—AI-driven software that extracts and enriches meaning from complex data—to make data more discoverable, collaborative, and useful.</p>
+                <p>With Kensho Solutions, knowledge workers quickly access the enterprise data they need and analyze, transform and shape it with our suite of tools, unlocking insights in hard-to-get data.</p>
+            </div>
+        </div>
+    );
+};
+
+export default AboutKenshoSolutions;
--- src/pages/Solution/Solution.js
+++ src/pages/Solution/Solution.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Boxhearder from '../../Components/Boxhearder/Boxhearder';
 import OurProduct from '../../Layouts/OurProduct/OurProduct';
 import Slutiondata from '../../Layouts/SlutionData/SlutionData';
+import AboutKenshoSolutions from "../../Layouts/AboutKenshoSolutions/AboutKenshoSolutions";
 const Solution = () => {
     return (
         <div>
             <Boxhearder/>
             <OurProduct/>
             <Slutiondata/>
+            <AboutKenshoSolutions/>
         </div>
     );
 };
